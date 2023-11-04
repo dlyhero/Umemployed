16baa479f50a8b3cc1f2fd9a73c6cd30a4d6b9e1
NyuydineBill <billleynyuy@gmail.com>
2023-11-04 16:47:58 +0100

    Added content to the README file

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# Umemployed
+UmEmployed
+UmEmployed is a Django-based web application designed to help job seekers find employment opportunities.
+
+Installation
+To get started with UmEmployed, follow the steps below:
+
+Clone the repository to your local machine:
+
+git clone https://github.com/your-username/UmEmployed.git
+
+
+Navigate to the project directory:
+
+cd UmEmployed
+
+Create a virtual environment (optional but recommended) to isolate your project dependencies:
+
+
+python3 -m venv venv
+
+
+Activate the virtual environment:
+
+For Windows:
+
+venv\Scripts\activate
+
+For Unix/macOS:
+
+
+source venv/bin/activate
+Install the project dependencies:
+
+
+pip install -r requirements.txt
+
+
+
+Rename the .env.example file to .env and update the database configuration with your PostgreSQL credentials:
+
+
+DATABASE_NAME=your_database_name
+DATABASE_USER=your_username
+DATABASE_PASSWORD=your_password
+DATABASE_HOST=your_host
+DATABASE_PORT=your_port
+
+
+Apply the database migrations:
+
+python manage.py migrate
+
+
+
+Start the development server:
+
+
+python manage.py runserver
+Open your web browser and visit http://localhost:8000 to access the UmEmployed application.
+
+Usage
+Create an account or log in with existing credentials.
+Explore job listings by category, location, or search for specific keywords.
+Apply for job openings by submitting your resume and cover letter.
+Manage your job applications and track their status.
+Update your profile information and preferences.
+Contributing
+Not allowed at the moment
+
+License
+This project is licensed under the MIT License.
+
+Acknowledgments
+This project was inspired by the need to simplify the job search process.
+Special thanks to the Django community for their excellent documentation and support.
---
+++ users/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .models import User
+from .forms import RegisterUserForm
+
+from resume.models import Resume
+from company.models import Company
+
+# login a user
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=email, password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            return redirect('dashboard')  # Update the target name to match the appropriate URL name
+        else:
+            messages.warning(request, 'Something went wrong')
+            return redirect('login')
+    else:
+        return render(request, 'users/login.html')
+
+# register applicant
+def register_applicant(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            var = form.save(commit=False)
+            var.is_applicant = True
+            var.username = var.email
+            var.save()
+            Resume.objects.create(user=var)
+            messages.info(request, 'Your account has been created successfully')
+            return redirect("login")  # Update the target name to match the appropriate URL name
+        else:
+            messages.warning(request, "Something went wrong")
+            return redirect('register-applicant')
+    else:
+        form = RegisterUserForm()
+        context = {'form': form}
+        return render(request, 'users/register_applicant.html', context)
+
+# register recruiter only
+def register_recruiter(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            var = form.save(commit=False)
+            var.is_recruiter = True
+            var.username = var.email
+            var.save()
+            Company.objects.create(user=var)
+            messages.info(request, 'Your account has been created successfully')
+            return redirect("login")
+        else:
+            messages.warning(request, "Something went wrong")
+            return redirect('register-recruiter')
+    else:
+        form = RegisterUserForm()
+        context = {'form': form}
+        return render(request, 'users/register_recruiter.html', context)
+# logout a user
+def logout_user(request):
+    logout(request)
+    messages.info(request, 'Your session has ended')
+    return redirect('logout')  # Update the target name to match the appropriate URL name
+
+def home(request):
+    return render(request, 'users/home.html')
