70f9f41e710291e4eea45c8ea69e00ebddde47c7
NyuydineBill <billleynyuy@gmail.com>
2024-07-13 03:01:06 +0100

    deployind to render

--- umemployed/settings.py
+++ umemployed/settings.py
@@ -104,20 +104,20 @@ WSGI_APPLICATION = 'umemployed.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 import os
 import dj_database_url    
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Use dj_database_url to parse DATABASE_URL environment variable
-db_from_env = dj_database_url.config(conn_max_age=600)
+# db_from_env = dj_database_url.config(conn_max_age=600)
 
-DATABASES = {
-    'default': db_from_env
-}
+# DATABASES = {
+#     'default': db_from_env
+# }
 
 
 
