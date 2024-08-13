2ec9e5a1762c746b55f1ea53c9d61b2e6d68fd8b
NyuydineBill <billleynyuy@gmail.com>
2024-08-13 23:25:34 +0100

    Trying to update to heroku

--- umemployed/settings.py
+++ umemployed/settings.py
@@ -5,6 +5,8 @@ import geopy
 import dotenv
 dotenv.load_dotenv()
 import dj_database_url  
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -283,3 +285,6 @@ AUTHENTICATION_BACKENDS = [
     'social_core.backends.google.GoogleOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+
+django_heroku.settings(locals())
