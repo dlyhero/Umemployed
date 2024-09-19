f75f60800dcb6a881854d6e75140001f9236a802
NyuydineBill <billleynyuy@gmail.com>
2024-09-19 14:09:26 +0100

    updated asgi file to include * for url

--- umemployed/asgi.py
+++ umemployed/asgi.py
@@ -1,5 +1,3 @@
-# your_project_name/asgi.py
-
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -8,16 +6,14 @@ from django.urls import path
 from messaging import consumers
 import notifications.routing
 
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'umemployed.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            notifications.routing.websocket_urlpatterns,
+            *notifications.routing.websocket_urlpatterns,
             path('ws/chat/<int:conversation_id>/', consumers.ChatConsumer.as_asgi()),
-
         ])
     ),
 })
