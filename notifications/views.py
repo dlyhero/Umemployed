253cce4890555c34956d42efa31dbde334f46b13
NyuydineBill <billleynyuy@gmail.com>
2024-09-29 14:14:04 +0100

    updated the header

--- notifications/views.py
+++ notifications/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Notification
 
@@ -12,11 +12,21 @@ def notifications(request):
         return render(request, 'notifications/applicant_notifications.html', {'notifications': notifications})
 
 
-def user_notifications(request):
-    notifications = request.user.notification_set.filter(is_read=False)
-    return render(request, 'dashboard/notifications.html', {'notifications': notifications})
 
-from django.shortcuts import redirect, get_object_or_404
+def user_notifications(request):
+    notifications = request.user.notification_set.order_by('-timestamp')
+    unread_notifications = notifications.filter(is_read=False)
+    total_notifications_count = notifications.count()
+    unread_notifications_count = unread_notifications.count()
+    
+    context = {
+        'notifications': notifications,
+        'total_notifications_count': total_notifications_count,
+        'unread_notifications_count': unread_notifications_count,
+    }
+    
+    return render(request, 'dashboard/notifications.html', context)
+
 
 def mark_notification_as_read(request, notification_id):
     notification = get_object_or_404(Notification, id=notification_id, user=request.user)
