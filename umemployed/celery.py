8bfcbb7232a13586c4bf970bc4352058333c91fe
NyuydineBill <billleynyuy@gmail.com>
2024-10-26 11:28:14 +0100

    Done resolving redis bugs and tested celery

--- umemployed/celery.py
+++ umemployed/celery.py
@@ -1,11 +1,16 @@
 from __future__ import absolute_import, unicode_literals
 import os
+import ssl
 from celery import Celery
-from django.conf import settings 
+from django.conf import settings
+
+# Load environment variables
+REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'umemployed.settings')
 
-app = Celery('umemployed')
+app = Celery('umemployed'
+)
 
 app.config_from_object(settings, namespace='CELERY')
 
@@ -14,4 +19,3 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
-    
